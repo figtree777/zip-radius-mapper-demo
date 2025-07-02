f76861b32a1b127cd7a5ecc0cac18f67165c14a0
figtree777 <maximusfigueras@gmail.com>
2025-07-02 19:12:33 -0400

    first commit

---
+++ dist/script.js
@@ -0,0 +1,3 @@
+https://unpkg.com/leaflet/dist/leaflet.js
+https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.5/xlsx.full.min.js
+https://cdnjs.cloudflare.com/ajax/libs/PapaParse/5.3.2/papaparse.min.js
